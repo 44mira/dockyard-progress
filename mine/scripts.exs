ee3dceaca3b62aad35a85d32355fd23d342cc814
44mira <legolaslada@gmail.com>
2024-01-24 13:05:19 +0800

    finish Anagram exercise

--- mine/scripts.exs
+++ mine/scripts.exs
@@ -1,8 +1,20 @@
 
-defmodule Test do
-  def parseString(""), do: []
-  def parseString(str) do
-    {n, rest} = Integer.parse(str)
-    [n | parseString(rest)]
+defmodule Anagram do
+  def solve(str) do
+    str
+    |> to_charlist
+    |> perms
+    |> Enum.map(&to_string/1)
+  end
+
+  defp rots(xs) do
+    xs
+    |> Stream.iterate(fn [h | t] -> t ++ [h] end)
+    |> Enum.take(length(xs))
+  end
+
+  defp perms([]), do: [[]]
+  defp perms([h | t]) do
+    perms(t) |> Enum.flat_map(&rots([h | &1]))
   end
 end
